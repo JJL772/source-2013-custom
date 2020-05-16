4e05f3b7f10eebf0f384262158c11493acc51b4b
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-05-15 19:07:58 -0700

    Update the env script

--- src/env.sh
+++ src/env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Change the prefix to whatever you want
-
-export INSTALL_PREFIX="$PWD/../game" 
+# MUST BE AN ABSOLUTE PATH
+export INSTALL_PREFIX="$HOME/Desktop/Projects/Source-Playground/game" 
 
 # Number of jobs to run make with
 export JOBS=8
+
