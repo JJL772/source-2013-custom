17eb6aec571c28672031184bfbe19533ca101b49
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-05-15 19:07:18 -0700

    Updated install scripts

--- src/gamedata/install.sh
+++ src/gamedata/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Install script for various gamedata files
-
-if [ -z $INSTALL_PREFIX ]; then
-	INSTALL_PREFIX=$(cat prefix.txt)
-fi
+pushd `dirname $0`
+source ../env.sh 
 
 cp -v entities/* "$INSTALL_PREFIX/bin"
 cp -v scripts/* "$INSTALL_PREFIX/"
+popd
--- src/gamedata/scripts/launch.sh
+++ src/gamedata/scripts/launch.sh
@@ -1,1 +1,3 @@
- 
+#!/bin/bash
+
+./hl2.sh -game mod_hl2mp -dev +sv_cheats 1 +sk_fraggrenade_debug 1 +map dm_lockdown 
