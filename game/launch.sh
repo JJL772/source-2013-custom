d8cb77b4f401becc6f79250a33e1ee4c5f05a8e3
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-06-06 02:28:59 -0700

    Update game

--- game/launch.sh
+++ game/launch.sh
@@ -1,12 +1,3 @@
+#!/bin/bash
 
-pushd $(dirname $0) 
-
-export LD_LIBRARY_PATH="$PWD/bin:$LD_LIBRARY_PATH"
-
-if [ ! -z $GAME_DEBUGGER]; then
-	"$GAME_DEBUGGER" ./hl2_linux -dev -windowed -w 1920 -h 1080 -game mod_hl2mp
-else
-	./hl2_linux -dev -windowed -w 1920 -h 1080 -game mod_hl2mp 
-fi 
-
-
+./hl2.sh -game mod_hl2mp -dev +sv_cheats 1 +sk_fraggrenade_debug 1 +map dm_lockdown 
