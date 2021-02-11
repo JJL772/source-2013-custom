ae96928325e56234ce2b958176516ee410af3abe
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-02-11 06:36:30 -0800

    Remove pointless scripts

--- src/relink-linux32.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pushd "`dirname $0`/build"
-source env.sh
-
-# Forces a relink
-find . -iname "*.o" | xargs touch 
-make -j$JOBS && make install 
-
-popd 
--- src/run-linux32.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source env.sh 
-
-pushd "$INSTALL_PREFIX"
-
-./launch.sh 
-
-popd 
