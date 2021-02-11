dd20215b2555e2a234c4a87998fe29ae6aca3d0d
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-02-11 09:06:02 -0800

    Another big update

---
+++ src/game/shared/managed/man_main.cpp
@@ -0,0 +1,52 @@
+#include "man_main.h"
+
+CManagedGameSystem::CManagedGameSystem()
+{
+	
+}
+
+
+bool CManagedGameSystem::Init()
+{
+	
+}
+void CManagedGameSystem::PostInit()
+{
+	
+}
+void CManagedGameSystem::Shutdown()
+{
+	
+}
+void CManagedGameSystem::LevelInitPreEntity()
+{
+	
+}
+void CManagedGameSystem::LevelInitPostEntity()
+{
+	
+}
+void CManagedGameSystem::LevelShutdownPreClearSteamAPIContext()
+{
+	
+}
+void CManagedGameSystem::LevelShutdownPreEntity()
+{
+	
+}
+void CManagedGameSystem::LevelShutdownPostEntity()
+{
+	
+}
+void CManagedGameSystem::OnSave()
+{
+	
+}
+void CManagedGameSystem::OnRestore()
+{
+	
+}
+void CManagedGameSystem::SafeRemoveIfDesired()
+{
+	
+}
