bb8836622df97397ab705b9325a2c4ddbe2fc55f
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-02-11 09:58:04 -0800

    Nuke cmake stuff

--- src/cmake_scripts/exe-base.cmake
+++
@@ -1,8 +0,0 @@
-#
-#
-# Base for all exes
-#
-#
-
-set(THIS_IS_A_EXE 1)
-include(${CMAKESCRIPTS_DIR}/project-base.cmake)
--- src/cmake_scripts/library-base.cmake
+++
@@ -1,8 +0,0 @@
-#
-#
-# Base for all libraries
-#
-#
-
-set(THIS_IS_A_LIBRARY 1)
-include(${CMAKESCRIPTS_DIR}/project-base.cmake)
