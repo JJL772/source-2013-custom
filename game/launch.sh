f5b3ea56432246401f4ed7fe5decffb6572558fd
JJL772 <jeremy.lorelli.1337@gmail.com>
2022-12-05 15:55:23 -0800

    hl2mp

--- game/launch.sh
+++ game/launch.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 
 export LD_PRELOAD="$PWD/bin/libtogl.so"
 export LD_LIBRARY_PATH="$PWD/bin"
-./hl2_linux -game mod_hl2mp -dev +sv_cheats 1 +map sdk_vehicles
+$GAME_DEBUGGER ./hl2_linux -game mod_hl2mp $@
